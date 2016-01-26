697654ec5ce682317dc186f384ba0e7732215a53
Ben Gundersen <ben@bengundersen.com>
2016-01-26 02:33:23 -0500

    playing with AsyncStorage

--- views/DashboardView.js
+++ views/DashboardView.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import React, {
+  AsyncStorage,
   Component,
   ScrollView,
   StyleSheet,
@@ -23,21 +24,41 @@ export default class DashboardView extends Component {
 
   componentWillMount() {
     console.log("test");
-    fetch('http://con-nexus.bgun.me/api/con/jcon2015', {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    }).then(resp => resp.json())
-      .then(data => {
-        console.log("resp", data);
-        this.setState({
-          con_data: data
+    let t = this;
+
+    AsyncStorage.getItem('con_data', function(err, resp) {
+      console.log("what was in storage?", resp);
+      if (resp) {
+        console.log("found old data");
+        t.setState({
+          con_data: JSON.parse(resp)
         });
-      })
-      .catch(err => {
-        console.warn(err);
-      });
+      } else {
+        fetch('http://con-nexus.bgun.me/api/con/jcon2015', {
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }
+        }).then(resp => resp.json())
+          .then(data => {
+            console.log("fetched data", data);
+            AsyncStorage.setItem('con_data', JSON.stringify(data), function() {
+              console.log("set new data");
+              t.setState({
+                con_data: data
+              });
+            });
+          })
+          .catch(err => {
+            console.warn(err);
+          });
+      }
+
+    })
+    .catch(function() {
+        console.error("error fetching data");
+    });
+
   }
 
   render() {
